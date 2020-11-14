c421ab0fbfcdc6f13c9ba7b163dcb3b2e8c3207f
Sunil Shah <sunilshah2416@gmail.com>
2020-11-14 08:55:54 +0545

    Whatsapp Container

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCFnbgafUOGn2uEBgSTK44No7inD13D8L8",
+    authDomain: "whatsapp-mern-717f1.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-717f1.firebaseio.com",
+    projectId: "whatsapp-mern-717f1",
+    storageBucket: "whatsapp-mern-717f1.appspot.com",
+    messagingSenderId: "852554332249",
+    appId: "1:852554332249:web:86ec2d1b450745123ad948",
+    measurementId: "G-S6BTG2H23S"
+};
